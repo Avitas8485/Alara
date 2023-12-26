73056924fd6f653c66c1b9ccb096b31e34bc9a70
Avity <irritas8485@gmail.com>
2023-12-26 02:29:37 -0500

    Remove morning routine and scheduler

--- hestia/routines/morning/morning_routine.py
+++ hestia/routines/morning/morning_routine.py
@@ -2,6 +2,7 @@ from hestia.tools.reports.news_report import NewsReport
 from hestia.tools.reports.weather_report import WeatherReport
 from hestia.text_to_speech.tts_utils import play_audio
 from datetime import datetime
+from prefect import task,flow
 import os
 from hestia.lib.hestia_logger import HestiaLogger
 
@@ -16,29 +17,38 @@ def generate_report(report_class):
         report.convert_summary_to_audio()
     except Exception as e:
         logger.error(f"Error generating report: {e}")
-    
-def news_report():
-    generate_report(NewsReport)
-    
-def weather_report():
-    generate_report(WeatherReport)
-    
-def morning_preparation():
-    news_report()
-    weather_report()
-    
+        
 def play_report(report_type):
     todays_date = datetime.now().strftime("%b %d, %Y")
     report_path = f"hestia/text_to_speech/outputs/{report_type}/{todays_date}{report_type}.wav"
     play_audio(report_path)
+
+@task    
+def news_report():
+    generate_report(NewsReport)
     
+@task    
+def weather_report():
+    generate_report(WeatherReport)
+
+@flow
+def morning_preparation():
+    news_report()
+    weather_report()
+
+    
+
+@task    
 def play_weather():
     play_report("weather_report")
-    
+@task    
 def play_news_details():
     play_report("news_report")
     
+
+@flow   
 def morning_presentation():
     play_weather()
     play_news_details()
     
+
