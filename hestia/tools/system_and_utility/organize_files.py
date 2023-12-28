8598325bbd997316256bd31b7278599b5e6e0ab7
Avity <irritas8485@gmail.com>
2023-12-27 22:29:53 -0500

    Refactor file organization code and add file monitoring

--- hestia/tools/system_and_utility/organize_files.py
+++ hestia/tools/system_and_utility/organize_files.py
@@ -1,6 +1,8 @@
 import os
 from shutil import move
 from pathlib import Path
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 root_dir = Path(os.path.expanduser('~')) / 'Downloads'
 
@@ -14,17 +16,22 @@ file_types = {
 
 # Create directories if they don't exist
 for dir_name in file_types.keys():
-    (root_dir / dir_name).mkdir(parents=True, exist_ok=True)
+    os.makedirs(os.path.join(root_dir, dir_name), exist_ok=True)
 
-# Move files to the respective folders, overwriting if needed
-for file_name in os.listdir(root_dir):
-    file_path = root_dir / file_name
-    if file_path.is_file() and not file_name.startswith('.') and not file_name == __file__:
-        file_ext = file_path.suffix
-        for dir_name, extensions in file_types.items():
-            if file_ext in extensions:
-                move(str(file_path), str(root_dir / dir_name / file_name))
-                break
-        else:
-            # If the file extension is not in the dictionary, move it to the 'others' directory
-            move(str(file_path), str(root_dir / 'others' / file_name))
+def organize_files():
+    # Move files to the respective folders, overwriting if needed
+    for file_name in os.listdir(root_dir):
+        file_path = os.path.join(root_dir, file_name)
+        if os.path.isfile(file_path) and not file_name.startswith('.') and not file_name == os.path.basename(__file__):
+            file_ext = os.path.splitext(file_path)[1]
+            for dir_name, extensions in file_types.items():
+                if file_ext in extensions:
+                    move(file_path, os.path.join(root_dir, dir_name, file_name))
+                    break
+            else:
+                # If the file extension is not in the dictionary, move it to the 'others' directory
+                move(file_path, os.path.join(root_dir, 'others', file_name))
+
+class MyHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        organize_files()
