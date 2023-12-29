15b1f60b3fb7cc67062325a1b642683dce90405c
Avity <irritas8485@gmail.com>
2023-12-29 14:22:11 -0500

    morning routine is now scheduled to run in a specific time

--- main.py
+++ main.py
@@ -7,13 +7,7 @@ from hestia.routines.morning.morning_routine import schedule_morning_routine, mo
 app = Flask(__name__)
 
 scheduler = SchedulerManager()
-#schedule_morning_routine()
-# lets add a job that executes immediately
-"""scheduler.add_job(morning_preparation,trigger="date",run_date=datetime.now())
-scheduler.add_job(morning_presentation,trigger="date",run_date=datetime.now() + timedelta(minutes=30))
-scheduler.start_scheduler()"""
-#morning_preparation()
-morning_presentation()
+schedule_morning_routine()
 
 
 @app.route("/")
