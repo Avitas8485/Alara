74db29fba049af631a608d0f15d6b0727e233f4a
Avity <irritas8485@gmail.com>
2023-12-31 15:49:05 -0500

    added calendar routine

--- hestia/routines/recurring/calendar.py
+++ hestia/routines/recurring/calendar.py
@@ -1,21 +1,43 @@
-from HESTIA.hestia.tools.system_and_utility.notifications.notifications import notify
-from HESTIA.hestia.tools.system_and_utility.google_calendar import GoogleCalendar
-from HESTIA.hestia.tools.system_and_utility.scheduler import SchedulerManager
+from typing import Dict
+from hestia.tools.system_and_utility.notifications.notifications import notify
+from hestia.tools.system_and_utility.google_calendar import GoogleCalendar
+from hestia.tools.system_and_utility.scheduler import SchedulerManager
 from datetime import datetime, timedelta
+from hestia.lib.hestia_logger import logger
 
+def get_events() -> list:
+    calendar = GoogleCalendar()
+    try:
+        events = calendar.get_events_list()
+    except Exception as e:
+        logger.error(f"Failed to get events: {e}")
+        events = []
+    return events
 
+def schedule_notification(scheduler: SchedulerManager, event: Dict):
+    try:
+        if "dateTime" in event["start"]:
+            event_start = datetime.fromisoformat(event["start"]["dateTime"])
+        else:
+            event_start = datetime.fromisoformat(event["start"]["date"]) + timedelta(hours=9)
+        notification_time = event_start - timedelta(minutes=60)
+        scheduler.add_job(send_notification, trigger="date", run_date=notification_time, args=[event])
+    except Exception as e:
+        logger.error(f"Failed to schedule notification: {e}")
 
-calendar = GoogleCalendar()
-events = calendar.get_events_list()
-
-scheduler = SchedulerManager()
-
-def send_notification(event):
+def send_notification(event: Dict):
     title = event["summary"]
     message = f"{event['summary']} at {event['start'].get('dateTime', event['start'].get('date'))}"
-    notify(title, message)
-    
-for event in events:
-    event_start = datetime.fromisoformat(event["start"].get("dateTime", event["start"].get("date")))
-    notification_time = event_start - timedelta(minutes=60)
-    scheduler.add_job(send_notification, trigger="date", run_date=notification_time, args=[event])
+    try:
+        notify(title, message)
+    except Exception as e:
+        logger.error(f"Failed to send notification: {e}")
+
+def main():
+    events = get_events()
+    scheduler = SchedulerManager()
+    for event in events:
+        schedule_notification(scheduler, event)
+
+if __name__ == "__main__":
+    main()
