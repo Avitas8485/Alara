22831f8544d52b77df468504af91d3f0f47e6c82
Esoteric Dreamer <117129159+Avitas8485@users.noreply.github.com>
2024-02-23 20:10:07 -0500

    changed hestia main logic

--- main.py
+++ main.py
@@ -1,76 +1,24 @@
-#from flask import Flask, render_template
-#from hestia.tools.system_and_utility.scheduler import SchedulerManager
-#from hestia.lib.hestia_logger import logger
-#from hestia.routines.morning.morning_routine import morning_preparation, morning_presentation
-
-from kivy.app import App
-from kivy.uix.label import Label
-from datetime import datetime
-import time
-from kivy.clock import Clock
-
-
-class Hestia(App):
-    
-    def build(self):
-        self.label = Label(text="00:00:00")
-        Clock.schedule_interval(self.update, 1)
-        return self.label
-    
-    def update(self, *args):
-        #self.label.text = time.strftime("%H:%M:%S")
-        self.label.text = datetime.now().strftime("%d %b %Y %H:%M:%S")
-        
-    def on_stop(self):
-        print("on_stop")
-        return True
-    
-    def on_pause(self):
-        print("on_pause")
-        return True
-    
-    def on_resume(self):
-        print("on_resume")
-        return True
-    
-    def on_start(self):
-        print("on_start")
-        return True
-    
-    
-    
-        
-if __name__ == "__main__":
-    Hestia().run()
-    
-    '''
-app = Flask(__name__)
-
-scheduler = SchedulerManager()
-scheduler.start_scheduler()
-def schedule_morning_routine():
-    """Schedule the morning routine."""
-    logger.info("Scheduling morning routine...")
-    scheduler.add_job(morning_preparation,trigger="cron",hour=7,minute=00)
-    scheduler.add_job(morning_presentation,trigger="cron",hour=8,minute=30)
-    logger.info("Morning routine scheduled.")
+from hestia.llm.llama_chat_completion import chat_completion
+from hestia.tts.piper_tts import PiperTTS
+from hestia.stt.whisper_stt import StreamHandler
+from hestia.stt.wakeword import WakeWord
 
 
 
 
+system_prompt = "Your name is Alara. You are an AI assistant that helps people with their daily tasks." 
+tts = PiperTTS()
+handler = StreamHandler()
+wake_word = WakeWord()
+if wake_word.wake_word_detection():
+    print("\033[96mWakeword detected..\033[0m")
+    user_prompt = handler.listen()
 
+    if isinstance(user_prompt, list):
+        user_prompt = ' '.join(user_prompt)
+    elif user_prompt is None:
+        user_prompt = ''
 
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-
-
-
-
-
-
-if __name__ == "__main__":
-    app.run(debug=False)
-    
-    '''
+    output = chat_completion(system_prompt, user_prompt)
+    print(output)
+    tts.synthesize(output)
