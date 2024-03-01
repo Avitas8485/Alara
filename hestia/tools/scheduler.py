fcd912b6d815a46a97f64eafb57a2ba3ba651890
Esoteric Dreamer <117129159+Avitas8485@users.noreply.github.com>
2024-03-01 00:03:29 -0500

    modify scheduler logic

--- hestia/tools/scheduler.py
+++ hestia/tools/scheduler.py
@@ -1,10 +1,9 @@
-from typing import Optional, Any, List
+from typing import Optional, Any, List, Callable
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from hestia.lib.hestia_logger import logger
-from uuid import uuid4
-
-
+import hashlib
 
 EXECUTORS = {
     'default': ThreadPoolExecutor(20),
@@ -17,27 +16,28 @@ JOB_DEFAULTS = {
     'misfire_grace_time': 500
 }
 
+STORAGE = {
+    'default': SQLAlchemyJobStore(url='sqlite:///jobs.sqlite')
+}
+
 class SchedulerManager:
     """Class to manage the scheduler.
     Attributes:
         scheduler: APscheduler instance"""
     def __init__(self):
-        self.scheduler = BackgroundScheduler(executors=EXECUTORS, job_defaults=JOB_DEFAULTS, daemon=True)
-        # assuming all jobs are rescheduled every time I start the app
-        self.scheduler.remove_all_jobs()
+        self.scheduler = BackgroundScheduler(executors=EXECUTORS, job_defaults=JOB_DEFAULTS, daemon=True, jobstores=STORAGE)
+        self.scheduler.start()
 
                 
-    def add_job(self, job_function: Any, job_id: Optional[str] = None, trigger: Optional[str] = None, **kwargs: Any) -> None:
+    def add_job(self, job_function: Callable, job_id: Optional[str] = None, trigger: Optional[str] = None, **kwargs: Any) -> None:
         """Add a job to the scheduler.
         Args:
             job_function: The function to run.
             job_id: The id of the job.
             trigger: The trigger for the job.
             kwargs: The keyword arguments for the job."""
-        # generate a job id if one is not provided
         if job_id is None:
-            job_id = str(uuid4())
-        # get all jobs from the scheduler
+            job_id = hashlib.md5(str(job_function).encode()).hexdigest()
         if self.scheduler.get_job(job_id=job_id):
             logger.info(f"Job with id {job_id} already exists.")
             return 
@@ -66,13 +66,18 @@ class SchedulerManager:
         except Exception as e:
             logger.error(f"Error getting jobs: {e}")
             return []        
-    def modify_job(self, job_id: str, **changes: Any) -> None:
+    def modify_job(self, job_id: str, job_function: Optional [Callable], trigger: Optional[str] = None, **changes: Any) -> None:
         """Modify a job.
         Args:
             job_id: The id of the job.
             changes: The changes to make to the job."""
+            
         try:
-            self.scheduler.modify_job(job_id, **changes)
+            # this is a workaround to modify the job
+            job = self.scheduler.get_job(job_id)
+            if job:
+                self.scheduler.remove_job(job_id)
+                self.scheduler.add_job(job_function, trigger=trigger, id=job_id,replace_existing=True, **changes)
         except Exception as e:
             logger.error(f"Error modifying job: {e}")
             return
