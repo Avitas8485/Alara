52d492be727245db886a2235b79d674c89007110
Esoteric Dreamer <117129159+Avitas8485@users.noreply.github.com>
2024-03-27 12:50:28 -0400

    refactored the code inherit from Tools and Toolkit

--- hestia/tools/ip_geolocation.py
+++ hestia/tools/ip_geolocation.py
@@ -1,5 +1,65 @@
 import requests
 import sys
+from hestia.tools.base_tool import Tool, ToolKit
+
+class IpAddressTool(Tool):
+    def __init__(self):
+        self.name = "ip_address"
+        self.description = "Get the IP address of the system"
+        self.usage = "ip_address"
+        self.dependencies = {}
+        
+    def run(self):
+        url = "https://api.ipify.org?format=json"
+        response = requests.request("GET", url)
+        ip_address = response.json()['ip']
+        return ip_address
+        
+class IpInfoTool(Tool):
+    def __init__(self):
+        self.name = "ip_info"
+        self.description = "Get basic information about an IP address"
+        self.usage = "ip_info [ip_address]"
+        self.dependencies = {"ip_address": IpAddressTool()}
+        
+    def run(self, ip_address: str= ""):
+        # check dependencies
+        if ip_address == "":
+            ip_address = self.dependencies['ip_address'].run()
+        url = f"http://ip-api.com/json/{ip_address}"
+        response = requests.request("GET", url)
+        geolocation = response.json()
+        return geolocation
+
+class IpGeolocationToolKit(ToolKit):
+    def __init__(self):
+        super().__init__()
+        self.add_tool(IpAddressTool())
+        self.add_tool(IpInfoTool())
+        
+    def __str__(self)-> str:
+        return "\n".join([str(tool) for tool in self.tools.values()])
+    
+    def run(self, tool_name: str, *args, **kwargs):
+        if self.check_dependencies(tool_name):
+            tool = self.tools[tool_name]
+            return tool.run(*args, **kwargs)
+        else:
+            print(f"Error running tool: {tool_name}")
+'''
+toolkit = ToolKit()
+toolkit.add_tool(IpAddressTool())
+toolkit.add_tool(IpInfoTool())
+print(toolkit)
+
+if __name__ == '__main__':
+    # test the tools
+    toolkit.run("ip_address", [])
+    toolkit.run("ip_info", [])
+    
+    
+ 
+  
 
 def get_ip_address() -> str:
     """Returns IP address of the system"""
@@ -28,4 +88,4 @@ if __name__ == '__main__':
     ISP: {geolocation['isp']}
     Organization: {geolocation['org']}
     Timezone: {geolocation['timezone']}
-    """)
+    """)'''
--- hestia/tools/time.py
+++ hestia/tools/time.py
@@ -1,37 +1,151 @@
 from datetime import datetime
 from dateutil.tz import gettz, tzlocal
-from ip_geolocation import get_geolocation
-
-def default_timezone():
-    return gettz(get_geolocation()['timezone'])
+from base_tool import Tool, ToolKit
 
 
-def set_timezone(timezone):
-    return gettz(timezone)
 
-def to_timezone(dt: datetime, tz):
-    """Convert a datetime object to a specified timezone."""
-    if dt.tzinfo:
-        return dt.astimezone(tz)
-    else:
-        return dt.replace(tzinfo=tz).astimezone(tz)
-
-def to_utc(dt):
-    """Convert a datetime object to UTC timezone."""
-    tzUTC = gettz("UTC")
-    return to_timezone(dt, tzUTC)
-
-def now_utc():
-    """Return the current time in UTC timezone."""
-    return to_utc(datetime.utcnow())
-   
-def to_local(dt):
-    """Convert a datetime object to the local timezone."""
-    tz = default_timezone()
-    return to_timezone(dt, tz)
+class CurrentTimeTool(Tool):
+    def __init__(self):
+        self.name = "current_time"
+        self.description = "Get current time"
+        self.usage = "current_time"
+        self.dependencies = {}
+        
+    def run(self)-> str:
+        return datetime.now().strftime("%H:%M:%S")
     
+class CurrentDateTool(Tool):
+    def __init__(self):
+        self.name = "current_date"
+        self.description = "Get current date"
+        self.usage = "current_date"
+        self.dependencies = {}
+        
+    def run(self)-> str:
+        return datetime.now().strftime("%d/%m/%Y")
+    
+class CurrentDatetimeTool(Tool):
+    def __init__(self):
+        self.name = "current_datetime"
+        self.description = "Get current datetime"
+        self.usage = "current_datetime"
+        self.dependencies = {}
+        
+    def run(self)-> str:
+        return datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    
+class CurrentTimezoneTool(Tool):
+    def __init__(self):
+        self.name = "current_timezone"
+        self.description = "Get current timezone"
+        self.usage = "current_timezone"
+        self.dependencies = {}
+        
+    def run(self) -> str:
+        return str(datetime.now(tzlocal()).tzinfo)
+    
+class SetTimezoneTool(Tool):
+    def __init__(self):
+        self.name = "set_timezone"
+        self.description = "Set the timezone"
+        self.usage = "set_timezone [timezone]"
+        self.dependencies = {}
+        
+    def run(self, timezone: str) -> str:
+        return str(gettz(timezone))
+    
+class ToLocalTimeTool(Tool):
+    def __init__(self):
+        self.name = "to_local_time"
+        self.description = "Convert time to local timezone"
+        self.usage = "to_local_time [datetime]"
+        self.dependencies = {}
+        
+    def run(self, dt: datetime) -> str:
+        return str(dt.astimezone(tzlocal()))
+    
+class ToUtcTimeTool(Tool):
+    def __init__(self):
+        self.name = "to_utc_time"
+        self.description = "Convert time to UTC timezone"
+        self.usage = "to_utc_time [datetime]"
+        self.dependencies = {}
+        
+    def run(self, dt: datetime) -> str:
+        return str(dt.astimezone(gettz("UTC")))
+    
+class ToSystemTimezoneTool(Tool):
+    def __init__(self):
+        self.name = "to_system_timezone"
+        self.description = "Convert time to system timezone"
+        self.usage = "to_system_timezone [datetime]"
+        self.dependencies = {}
+        
+    def run(self, dt: datetime) -> str:
+        return str(dt.astimezone(tzlocal()))
+    
+class TimeDifferenceTool(Tool):
+    def __init__(self):
+        self.name = "time_difference"
+        self.description = "Get the time difference between two datetime objects"
+        self.usage = "time_difference [datetime1] [datetime2]"
+        self.dependencies = {}
+        
+    def run(self, dt1: datetime, dt2: datetime) -> str:
+        return str(dt1 - dt2)
+    
+class FormatTimeTool(Tool):
+    def __init__(self):
+        self.name = "format_time"
+        self.description = "Format the time string"
+        self.usage = "format_time [datetime] [format]"
+        self.dependencies = {}
+        
+    def run(self, dt: datetime, format="%d/%m/%Y %H:%M:%S") -> str:
+        return dt.strftime(format)
+    
+class ParseTimeTool(Tool):
+    def __init__(self):
+        self.name = "parse_time"
+        self.description = "Parse the time string"
+        self.usage = "parse_time [time_str] [format]"
+        self.dependencies = {}
+        
+    def run(self, time_str: str, format="%d/%m/%Y %H:%M:%S") -> str:
+        return str(datetime.strptime(time_str, format))
+    
+class TimeToolKit(ToolKit):
+    def __init__(self):
+        super().__init__()
+        self.add_tool(CurrentTimeTool())
+        self.add_tool(CurrentDateTool())
+        self.add_tool(CurrentDatetimeTool())
+        self.add_tool(CurrentTimezoneTool())
+        self.add_tool(SetTimezoneTool())
+        self.add_tool(ToLocalTimeTool())
+        self.add_tool(ToUtcTimeTool())
+        self.add_tool(ToSystemTimezoneTool())
+        self.add_tool(TimeDifferenceTool())
+        self.add_tool(FormatTimeTool())
+        self.add_tool(ParseTimeTool())
+        
+    def __str__(self)-> str:
+        return "\n".join([str(tool) for tool in self.tools.values()])
+    
+toolkit = TimeToolKit()
+print(toolkit)
 
-def to_system_timezone(dt):
-    """Convert a datetime object to the system's local timezone."""
-    tz = tzlocal()
-    return to_timezone(dt, tz)
+if __name__ == '__main__':
+    # test the tools
+    print(toolkit.run("current_time"))
+    print(toolkit.run("current_date"))
+    print(toolkit.run("current_datetime"))
+    print(toolkit.run("current_timezone"))
+    print(toolkit.run("set_timezone", "Asia/Kolkata"))
+    print(toolkit.run("to_local_time", datetime.now()))
+    print(toolkit.run("to_utc_time", datetime.now()))
+    print(toolkit.run("to_system_timezone", datetime.now()))
+    print(toolkit.run("time_difference", datetime.now(), datetime.now()))
+    print(toolkit.run("format_time", datetime.now()))
+    print(toolkit.run("parse_time", "01/01/2022 12:00:00"))
+    
