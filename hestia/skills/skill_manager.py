f0997162ef32bb437bd3a10ba1c501ed623557ab
Esoteric Dreamer <117129159+Avitas8485@users.noreply.github.com>
2024-04-10 01:03:34 -0400

    added pydantic models creation for skills

--- hestia/skills/skill_manager.py
+++ hestia/skills/skill_manager.py
@@ -1,6 +1,7 @@
 import importlib
 from hestia.skills.base_skill import NotImplementError, Skill
 import json
+from hestia.llm.grammar.pydantic_models_to_grammar import create_dynamic_model_from_function
 
 class SkillManager:
     def __init__(self) -> None:
@@ -16,11 +17,15 @@ class SkillManager:
             skill_name = self.skills_map.get(intent, intent)
             module = importlib.import_module(f"{self.skill_module_path}{intent}")
             skill_class = getattr(module, skill_name)
-            self.skills[intent] = skill_class()
+            self.skills[intent] = skill_class
         except (ImportError, AttributeError) as e:
             print(f"Skill {intent} not found {e}")
             
-
+    def add_skill(self, skill: Skill, skill_name: str):
+        self.skills[skill_name] = skill
+        
+    def get_skill(self, skill_name):
+        return self.skills.get(skill_name)
 
     def call_skill(self, skill_name, *args, **kwargs):
         skill = self.skills.get(skill_name)
@@ -29,6 +34,17 @@ class SkillManager:
             skill = self.skills.get(skill_name)
         if skill is not None:
             return skill(*args, **kwargs)
+        else:
+            raise NotImplementError(skill_name)
+        
+    def create_model(self, skill_name, feature_name):
+        skill = self.skills.get(skill_name)
+        if skill is None:
+            self.load_skill(skill_name)
+            skill = self.skills.get(skill_name)
+        if skill is not None:
+            feature = getattr(skill, feature_name)
+            return create_dynamic_model_from_function(feature)
         else:
             raise NotImplementError(skill_name)
         
