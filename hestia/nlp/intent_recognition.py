6fe8f41e0c9984bc8a67c4e3e77b68672116f6ae
Esoteric Dreamer <117129159+Avitas8485@users.noreply.github.com>
2024-04-11 01:36:31 -0400

    added type hinting, docstrings and logging statements

--- hestia/nlp/intent_recognition.py
+++ hestia/nlp/intent_recognition.py
@@ -7,22 +7,38 @@ from hestia.lib.hestia_logger import logger
 
 class IntentRecognition(metaclass=Singleton):
     def __init__(self):
+        """Initialize the intent recognition pipeline.
+        The pipeline uses the zero-shot-classification model from the transformers library."""
         self.classifier = pipeline(task="zero-shot-classification", model="facebook/bart-large-mnli")
         with open("hestia/nlp/intents.json", "r") as file:
             self.intents = json.load(file)
             
-    def classify_intent(self, text: str, intents, current_intent: dict, parent_intent=None):
+    def classify_intent(self, text: str, intents, current_intent: dict, parent_intent=None)->str:
+        """Classify the intent of a given text.
+        This function is recursive, and will classify the intent of a text based on the sub-intents of the current intent.
+        Args:
+            text (str): The text to classify.
+            intents (dict): The intents dictionary.
+            current_intent (dict): The current intent.
+            parent_intent (str): The parent intent.
+        Returns:
+            str: The classified intent."""
         candidate_labels = [key["name"] for key in current_intent]
         result = self.classifier(text, candidate_labels)
 
         for key in current_intent:
             if result['labels'][0] == key["name"]: # type: ignore
                 if "sub_intents" in key:
-                    return parent_intent, self.classify_intent(text, intents, key["sub_intents"], key["name"])
+                    return parent_intent, self.classify_intent(text, intents, key["sub_intents"], key["name"]) # type: ignore
         logger.info(f"Intent: {parent_intent}, Sub-intent: {result['labels'][0]}") # type: ignore
         return parent_intent, result['labels'][0] # type: ignore
     
-    def get_intent(self, text: str):
+    def get_intent(self, text: str)->str:
+        """Get the intent of a given text.
+        Args:
+            text (str): The text to classify.
+        Returns:
+            str: The classified intent."""
         candidate_labels = [key for key in self.intents for key in self.intents[key]]
         general_intents = [key["name"] for key in candidate_labels]
         result = self.classifier(text, general_intents)
--- hestia/skills/skill_manager.py
+++ hestia/skills/skill_manager.py
@@ -2,24 +2,27 @@ import importlib
 from hestia.skills.base_skill import NotImplementError, Skill
 import json
 from hestia.llm.grammar.pydantic_models_to_grammar import create_dynamic_model_from_function
+import logging
+from typing import Dict
+logger = logging.getLogger(__name__)
 
 class SkillManager:
     def __init__(self) -> None:
         self.skill_module_path = "hestia.skills."
-        self.skills = {}
+        self.skills: Dict[str, Skill] = {}
         with open("hestia/skills/skills.json") as f:
-            self.skills_map: dict = json.load(f)
+            self.skills_map: Dict[str, str] = json.load(f)
         for skill in self.skills_map:
             self.load_skill(skill)
         
-    def load_skill(self, intent):
+    def load_skill(self, intent: str):
         try:
             skill_name = self.skills_map.get(intent, intent)
             module = importlib.import_module(f"{self.skill_module_path}{intent}")
             skill_class = getattr(module, skill_name)
             self.skills[intent] = skill_class
         except (ImportError, AttributeError) as e:
-            print(f"Skill {intent} not found {e}")
+            logger.warning(f"Skill {intent} not implemented.")
             
     def add_skill(self, skill: Skill, skill_name: str):
         self.skills[skill_name] = skill
@@ -36,8 +39,8 @@ class SkillManager:
             return skill(*args, **kwargs)
         else:
             raise NotImplementError(skill_name)
-        
-    def create_model(self, skill_name, feature_name):
+    
+    def create_model(self, skill_name: str, feature_name: str):
         skill = self.skills.get(skill_name)
         if skill is None:
             self.load_skill(skill_name)
