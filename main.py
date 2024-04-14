df5e5114faad4763bd4d950791b18179d5e1e5a2
Esoteric Dreamer <117129159+Avitas8485@users.noreply.github.com>
2024-04-13 21:32:58 -0400

    minor changes

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ from hestia.agent.agent import Agent
 
 agent = Agent()
 agent.run()
+
+
+'''
+from hestia.skills.skill_manager import SkillManager
+
+skill_manager = SkillManager()
+skill_manager.call_feature("current_weather")'''
