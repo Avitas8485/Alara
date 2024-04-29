20ce62b856cb6e7b2f1c07cd9b4f90c8051b2e16
Esoteric Dreamer <117129159+Avitas8485@users.noreply.github.com>
2024-04-28 21:46:59 -0400

    minor refactoring

--- alara/llm/llm_engine.py
+++ alara/llm/llm_engine.py
@@ -1,12 +1,14 @@
 from alara.llm.llama_chat_completion import LlamaChatCompletion
 from alara.llm.llama_cpp_server import LlmServer
+from alara.lib.singleton import Singleton
+from alara.lib.logger import logger
 from enum import Enum
 
 class LlmType(Enum):
     llama_server = 'llama_server'
     llama_cpp = 'llama_cpp'
 
-class LlmEngine:
+class LlmEngine(metaclass=Singleton):
     @staticmethod
     def load_llm(llm_type: LlmType=LlmType.llama_server):
         if llm_type == LlmType.llama_server:
