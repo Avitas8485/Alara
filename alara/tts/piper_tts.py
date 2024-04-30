bcad5f9eb5036f3c28da75a417f3206689cb1cd3
Esoteric Dreamer <117129159+Avitas8485@users.noreply.github.com>
2024-04-30 01:38:03 -0400

    minor changes

--- alara/tts/piper_tts.py
+++ alara/tts/piper_tts.py
@@ -118,18 +118,4 @@ class PiperTTS(BaseTTS):
         
         return self.output_file
     
-
-def main():
-    text = """It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
-    The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
-    Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.
-    Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
     
-    It is 9:00 AM. Today is 02/23/2004 and the weather is 72 degrees Fahrenheit."""
-    piper = PiperTTS()
-    piper.synthesize(text)
-    piper.synthesize_to_file(text)
- 
-
-if __name__ == "__main__":
-    main()
