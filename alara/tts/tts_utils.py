145cd394ed7d3ffccba07e520421db2a96a91163
Esoteric Dreamer <117129159+Avitas8485@users.noreply.github.com>
2024-05-04 15:49:57 -0400

    minor changes

--- alara/tts/tts_utils.py
+++ alara/tts/tts_utils.py
@@ -2,7 +2,6 @@ import pyaudio
 import wave
 from typing import List
 import textwrap
-import spacy
 
 def play_audio(audio_path):
         """Play the audio."""   
@@ -22,47 +21,3 @@ def play_audio(audio_path):
         except Exception as e:
             print(f"Error: {e}")
     
-def get_spacy_lang(lang):
-    """Get the spacy language model.
-    Args:
-        lang: The language code.
-    Returns:
-        spacy.lang: The spacy language model."""
-    return spacy.blank(lang)
-
-def split_sentence(text, lang, text_split_length=250):
-    """Preprocess the input text"""
-    text_splits = []
-    if text_split_length is not None and len(text) >= text_split_length:
-        text_splits.append("")
-        nlp = get_spacy_lang(lang)
-        nlp.add_pipe("sentencizer")
-        doc = nlp(text)
-        for sentence in doc.sents:
-            if len(text_splits[-1]) + len(str(sentence)) <= text_split_length:
-                # if the last sentence + the current sentence is less than the text_split_length
-                # then add the current sentence to the last sentence
-                text_splits[-1] += " " + str(sentence)
-                text_splits[-1] = text_splits[-1].lstrip()
-            elif len(str(sentence)) > text_split_length:
-                # if the current sentence is greater than the text_split_length
-                for line in textwrap.wrap(
-                    str(sentence),
-                    width=text_split_length,
-                    drop_whitespace=True,
-                    break_on_hyphens=False,
-                    tabsize=1,
-                ):
-                    text_splits.append(str(line))
-            else:
-                text_splits.append(str(sentence))
-
-        if len(text_splits) > 1:
-            if text_splits[0] == "":
-                del text_splits[0]
-    else:
-        text_splits = [text.lstrip()]
-
-    return text_splits
-
-
